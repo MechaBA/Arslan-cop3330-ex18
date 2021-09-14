cfb184417fff4fc3d4b5f5fcee706a3a85545d68
MechaBA <berkay_arslan@live.com>
2021-09-14 00:27:40 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Berkay Arslan
+ */
+
+package org.example;
+import java.util.*;
+public class App {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Press C to convert from Fahrenheit to Celsius.");
+            System.out.println("Press F to convert from Celsius to Fahrenheit.");
+            System.out.print("Your choice: ");
+            char choice = in.next().toUpperCase().charAt(0);
+            if (choice == 'C')
+            {
+                System.out.print("Enter temperature in Fahrenheit: ");
+                double temp = in.nextDouble();
+                double result = (temp - 32) / 1.8;
+                System.out.println("Temperature in Celsius: " + result + ".");
+            }
+            else
+            {
+                System.out.print("Enter temperature in Celsius: ");
+                double temp = in.nextDouble();
+                double result = (temp * 1.8) + 32;
+                System.out.println("Temperature in Fahrenheit: " + result + ".");
+            }
+        }
+}
